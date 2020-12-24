9159b1d241ca45f1351e1f8166e7d7ac6fcc843a
jamesgov <blindmice1o3@gmail.com>
2020-12-24 13:00:54 -0800

    feat: Implement DvdController to store and retrieve data from DvdDB
    Able to POST new Dvd objects into the database. Can also GET Dvd objects by specifying an "id" for the path variable.

--- demo/src/main/java/com/example/demo/service/dvdlibrary/controller/DvdController.java
+++ demo/src/main/java/com/example/demo/service/dvdlibrary/controller/DvdController.java
@@ -1,27 +1,38 @@
 package com.example.demo.service.dvdlibrary.controller;
 
 import com.example.demo.service.dvdlibrary.model.Dvd;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.service.dvdlibrary.repository.DvdRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class DvdController {
-    private List<Dvd> dvds = new ArrayList<>();
+    private DvdRepository repository;
 
-    public DvdController() {}
-
-    @PostMapping(path="/dvd")
-    public void addDvd(@RequestBody Dvd dvd) {
-        dvds.add(dvd);
+    @Autowired
+    public DvdController(DvdRepository repository) {
+        this.repository = repository;
     }
 
-    @GetMapping("/dvds")
-    public List<Dvd> displayDvds() {
-        return dvds;
+    @PostMapping(path="/dvds", consumes="application/json")
+    public void postDvd(@RequestBody Dvd dvd) {
+        System.out.println("Adding to DvdDB: " + repository.save(dvd));
+    }
+
+    @GetMapping(path="/dvds/{id}")
+    public Dvd getDvd(@PathVariable int id) {
+        Optional<Dvd> dvdOptional = repository.findById(id);
+
+        if (dvdOptional.isPresent()) {
+            System.out.println("Getting from DvdDB: " + dvdOptional.get());
+            return dvdOptional.get();
+        } else {
+            System.out.println("Get request failed (no dvd with this \"id\"): " + id);
+            return null;
+        }
     }
 }
