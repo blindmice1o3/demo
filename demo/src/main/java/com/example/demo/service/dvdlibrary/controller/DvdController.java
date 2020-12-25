d0b97473c4c39948d318762fde4489d566fc1853
jamesgov <blindmice1o3@gmail.com>
2020-12-24 21:10:54 -0800

    feat: Add more request handler methods to DvdController

--- demo/src/main/java/com/example/demo/service/dvdlibrary/controller/DvdController.java
+++ demo/src/main/java/com/example/demo/service/dvdlibrary/controller/DvdController.java
@@ -5,7 +5,6 @@ import com.example.demo.service.dvdlibrary.repository.DvdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,21 +17,62 @@ public class DvdController {
         this.repository = repository;
     }
 
-    @PostMapping(path="/dvds", consumes="application/json")
-    public void postDvd(@RequestBody Dvd dvd) {
-        System.out.println("Adding to DvdDB: " + repository.save(dvd));
+    // Aggregate root
+
+    @GetMapping(path="/dvds")
+    List<Dvd> getAll() {
+        System.out.println("Getting all Dvd instances from DvdDB");
+
+        return repository.findAll();
     }
+
+    @PostMapping(path="/dvds", consumes="application/json")
+    public Dvd postDvd(@RequestBody Dvd newDvd) {
+        System.out.println(String.format("Posting %s to DvdDB", newDvd));
+
+        return repository.save(newDvd);
+    }
+
+    // Single item
 
     @GetMapping(path="/dvds/{id}")
     public Dvd getDvd(@PathVariable int id) {
         Optional<Dvd> dvdOptional = repository.findById(id);
 
         if (dvdOptional.isPresent()) {
-            System.out.println("Getting from DvdDB: " + dvdOptional.get());
+            System.out.println(String.format("Getting Dvd with \"id\":%d from DvdDB", id));
             return dvdOptional.get();
         } else {
-            System.out.println("Get request failed (no dvd with this \"id\"): " + id);
+            System.out.println(String.format("Get request else-clause (no dvd with \"id\":%d)", id));
+            // TODO: Create an ExceptionHandler class annotated with @RestControllerAdvice
             return null;
         }
+    }
+
+    @PutMapping(path="/dvds/{id}")
+    public Dvd replaceDvd(@RequestBody Dvd newDvd, @PathVariable int id) {
+        Optional<Dvd> dvdOptional = repository.findById(id);
+
+        if (dvdOptional.isPresent()) {
+            Dvd dvd = dvdOptional.get();
+
+            System.out.println(String.format("Replacing %s from DvdDB with %s", dvd, newDvd));
+
+            dvd.setTitle(newDvd.getTitle());
+            dvd.setAvailable(newDvd.isAvailable());
+
+            return repository.save(dvd);
+        } else {
+            newDvd.setId(id);
+
+            System.out.println(String.format("Put request else-clause (no dvd with \"id\":%d) (not replacing, just simply posting %s to DvdDB using the next available id)", id, newDvd));
+            return repository.save(newDvd);
+        }
+    }
+
+    @DeleteMapping(path="/dvds/{id}")
+    public void deleteDvd(@PathVariable int id) {
+        System.out.println("Deleting Dvd with \"id\":" + id + " from DvdDB");
+        repository.deleteById(id);
     }
 }
